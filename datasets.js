e597bd5efa119c8bd6c28552c07a974de0e4e1d6
mclenz2018 <clenzdevapp@gmail.com>
2021-06-12 10:52:37 +0200

    11062021

--- datasets.js
+++ datasets.js
@@ -1,4 +1,4 @@
-url='https://www.kaggle.com/apratim87/housingdata/download'
+url='http://localhost:5500/data/housing.csv';
 
 function* numGenerator(){
     const max = 6;
@@ -13,9 +13,10 @@ function* numGenerator(){
 async function datasetsCreate(){
     tf.setBackend('cpu');
 
-    //const ds = tf.data.array([1,2,3,4,5,6]);
+    const ds = tf.data.array([1,2,3,4,5,6]);
     //const ds = tf.data.generator(numGenerator);
-    const ds = tf.data.csv();
+    //const ds = tf.data.csv(url);
+    //console.log(await ds.columnNames());
 
     //Asynchroner Aufruf einer selbst programmierten Funktion
     await ds.forEachAsync(e => console.log(e));
